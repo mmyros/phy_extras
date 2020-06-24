dca1e70b2606b421bbe46eb4718f9958df70c6e8
maxym Myroshnychenko <mmyros@gmail.com>
2020-06-24 16:20:22 -0400

    CLI interface and parallel fix for phy

--- dotphy/plugins/mmy_cluster_metrics.py
+++ dotphy/plugins/mmy_cluster_metrics.py
@@ -53,13 +53,6 @@ class ClusterMetricsPlugin(IPlugin):
 
             return spike_times, spike_clusters
 
-        def isi_viol(cluster_id):
-            spike_train, spike_clusters = get_data(cluster_id)
-            # TODO minmax should really be on all spiketimes
-            return spike_io.QualityMetrics.isi_violations(spike_train, np.min(spike_train), np.max(spike_train),
-                                                          params['isi_threshold'],
-                                                          params['min_isi'])[0]
-
         def presence_ratio(cluster_id):
             spike_train, spike_clusters = get_data(cluster_id)
             return spike_io.QualityMetrics.presence_ratio(spike_train, np.min(spike_train), np.max(spike_train), )
@@ -69,54 +62,75 @@ class ClusterMetricsPlugin(IPlugin):
             amplitudes = controller.get_spike_raw_amplitudes(spike_ids)
             return spike_io.QualityMetrics.amplitude_cutoff(amplitudes)
 
+        def isi_viol(cluster_id):
+            spike_train, spike_clusters = get_data(cluster_id)
+            # TODO minmax should really be on all spiketimes
+            return spike_io.QualityMetrics.isi_violations(spike_train, np.min(spike_train), np.max(spike_train),
+                                                          params['isi_threshold'],
+                                                          params['min_isi'])[0]
+
         controller.cluster_metrics['isiviol'] = controller.context.memcache(isi_viol)
         controller.cluster_metrics['pres'] = controller.context.memcache(presence_ratio)
         controller.cluster_metrics['ampcut'] = controller.context.memcache(amplitude_cutoff)
         # =============================================================================================
         # The following necessitates loading of all features, which is more complicated and time consuming.
-        # TODO move to a different python file
 
-        (spike_times, spike_clusters, spike_templates, amplitudes, templates,
-         channel_map, clusterIDs, cluster_quality, pc_features, pc_feature_ind) = spike_io.IO.load_kilosort_data(
-            controller.dir_path, 3e4, False, include_pcs=True)
-        total_units = np.max(spike_clusters) + 1
-        (isolation_distance, l_ratio, d_prime, nn_hit_rate,
-         nn_miss_rate) = spike_io.Wrappers.calculate_pc_metrics(spike_clusters,
-                                                                spike_templates,
-                                                                total_units,
-                                                                pc_features,
-                                                                pc_feature_ind,
-                                                                params['num_channels_to_compare'],
-                                                                params['max_spikes_for_unit'],
-                                                                params['max_spikes_for_nn'],
-                                                                params['n_neighbors'],
-                                                                do_parallel=False)
+        # (spike_times, spike_clusters, spike_templates, amplitudes, templates,
+        #  channel_map, clusterIDs, cluster_quality, pc_features, pc_feature_ind) = spike_io.IO.load_kilosort_data(
+        #     controller.dir_path, 3e4, False, include_pcs=True)
+        import pandas as pd
+        # Uncomment this if you don't want to recompute every time phy loads
+        os.system('python ~/.phy/plugins/spike_io.py --do_drift=0')
+        try:
+            df = pd.read_csv(os.path.join(controller.dir_path, 'quality_metrics.csv'))
+        except FileNotFoundError:
+            # Force recalculation if spike measures do not exist
 
-        #TODO instead of above, run spike_io.main (thorough os.system if you must), save, and load
+            os.system('python ~/.phy/plugins/spike_io.py --do_drift=0')
 
+        # total_units = np.max(spike_clusters) + 1
+        # (isolation_distance, l_ratio, d_prime, nn_hit_rate,
+        #  nn_miss_rate) = spike_io.Wrappers.calculate_pc_metrics(spike_clusters,
+        #                                                         spike_templates,
+        #                                                         total_units,
+        #                                                         pc_features,
+        #                                                         pc_feature_ind,
+        #                                                         params['num_channels_to_compare'],
+        #                                                         params['max_spikes_for_unit'],
+        #                                                         params['max_spikes_for_nn'],
+        #                                                         params['n_neighbors'],
+        #                                                         do_parallel=False)
+        #
+        # silhouette_scores = spike_io.Wrappers.calculate_silhouette_score(spike_clusters,
+        #                                                                  pc_features,
+        #                                                                  pc_feature_ind,
+        #                                                                  params['n_silhouette'],
+        #                                                                  do_parallel=False)
+
+        # instead of above, run spike_io.main (thorough os.system if you must), save, and load
         # unpickle problems due to QT in phy. so cant use joblib for parallel
 
-        controller.cluster_metrics['idist'] = controller.context.memcache(lambda cluster_id: np.round(isolation_distance[
-            np.unique(spike_clusters) == cluster_id],2))
-        controller.cluster_metrics['lrat'] = controller.context.memcache(lambda cluster_id: np.round(l_ratio[
-            np.unique(spike_clusters) == cluster_id],2))
-        controller.cluster_metrics['miss'] = controller.context.memcache(lambda cluster_id: np.round(nn_miss_rate[
-            np.unique(spike_clusters) == cluster_id],2))
-        controller.cluster_metrics['hit'] = controller.context.memcache(lambda cluster_id: np.round(nn_hit_rate[
-            np.unique(spike_clusters) == cluster_id],2))
-        controller.cluster_metrics['dpr'] = controller.context.memcache(lambda cluster_id: np.round(d_prime[
-            np.unique(spike_clusters) == cluster_id],2))
-
-
-        # def silhouette_score(cluster_id):
-        #     the_silhouette_score = spike_io.Wrappers.calculate_silhouette_score(spike_clusters,
-        #                                                                         pc_features,
-        #                                                                         pc_feature_ind,
-        #                                                                         params['n_silhouette'],
-        #                                                                         do_parallel=False)
-        #     return the_silhouette_score
-        # controller.cluster_metrics['sil'] = controller.context.memcache(silhouette_score)
+        # Insert each measure into phy
+        for measure in df.columns:
+            if measure=='cluster_id':
+                continue
+            controller.cluster_metrics[measure[:5]] = controller.context.memcache(
+                lambda cluster_id: np.round(df[measure][np.unique(df['cluster_id']) == cluster_id].values[0], 2))
         #
+        # controller.cluster_metrics['idist'] = controller.context.memcache(
+        #     lambda cluster_id: np.round(df['isolation_distance'][np.unique(df['cluster_id']) == cluster_id].values[0],
+        #                                 2))
+        # controller.cluster_metrics['lrat'] = controller.context.memcache(
+        #     lambda cluster_id: np.round(df['l_ratio'][np.unique(df['cluster_id']) == cluster_id].values[0], 2))
+        # controller.cluster_metrics['miss'] = controller.context.memcache(
+        #     lambda cluster_id: np.round(df['nn_miss_rate'][np.unique(df['cluster_id']) == cluster_id].values[0], 2))
+        # controller.cluster_metrics['hit'] = controller.context.memcache(
+        #     lambda cluster_id: np.round(df['nn_hit_rate'][np.unique(df['cluster_id']) == cluster_id].values[0], 2))
+        # controller.cluster_metrics['dpr'] = controller.context.memcache(
+        #     lambda cluster_id: np.round(df['d_prime'][np.unique(df['cluster_id']) == cluster_id].values[0], 2))
+        # controller.cluster_metrics['sil'] = controller.context.memcache(
+        #     lambda cluster_id: np.round(df['silhouette_score'][np.unique(df['cluster_id']) == cluster_id].values[0], 2))
+
         # def drift_metrics(cluster_id):
         #     max_drift, cumulative_drift = spike_io.Wrappers.calculate_drift_metrics(spike_times,
         #                                                                             spike_clusters,
