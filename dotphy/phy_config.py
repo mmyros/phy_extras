dc2423d86b1af4c1264add9573f49348316201d3
maxym Myroshnychenko <mmyros@gmail.com>
2020-06-26 15:33:17 -0400

    cleanup

--- dotphy/phy_config.py
+++ dotphy/phy_config.py
@@ -9,10 +9,6 @@ from phy import IPlugin
 #     def attach_to_cli(self, cli):
 #         # you can create phy subcommands here with click
 #         pass
-from phy import IPlugin, Bunch
-from phy.cluster.views import HistogramView
 
 c = get_config()
 c.Plugins.dirs = [r'/home/m/.phy/plugins']
-# list of plugin names to load in the TemplateGUIExampleClusterStatsPlugin:
-c.TemplateGUI.plugins = ['RawDataFilterPluginMeanAndHighpass','ClusterMetricsPlugin']
